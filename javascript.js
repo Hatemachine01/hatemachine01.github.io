d8efa8e34b7149ca7aed49eb749233e702c9e7da
Julio <juliodevasquez@gmail.com>
2017-09-12 20:28:09 -0500

    added project navigation

--- javascript.js
+++ javascript.js
@@ -3,5 +3,10 @@ $( document ).ready(function() {
     $(".contact").show();
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
     });
-});
+    $(".project-trigger").click(function(){
+    $(".next-project").show();
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    });
 
+
+});
